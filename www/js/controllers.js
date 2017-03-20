69f107ea369eed574832e0c45d048be673123ed0
Wladimir Chopite <wchopite@gmail.com>
2017-03-20 14:20:55 -0400

    Inicio upload de imagen

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -258,10 +258,111 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('AlbumCtrl', function($scope,GenderFactory, ArtistFactory,AlbumFactory){
+
+
+
+
+
+
+
+
+
+
+
+.controller('AlbumCtrl', function($scope,GenderFactory, ArtistFactory,AlbumFactory,
+ $cordovaCamera, $cordovaFile, $cordovaFileTransfer, $cordovaDevice, $ionicPopup, $cordovaActionSheet){
 
   $genders = [];
   $artists = [];
+
+  $scope.image = null;
+
+  $scope.showAlert = function(title, msg) {
+    var alertPopup = $ionicPopup.alert({
+      title: title,
+      template: msg
+    });
+  };
+
+  $scope.selectPicture = function(sourceType) {
+  var options = {
+    quality: 100,
+    destinationType: Camera.DestinationType.FILE_URI,
+    sourceType: sourceType,
+    saveToPhotoAlbum: false
+  };
+ 
+  $cordovaCamera.getPicture(options).then(function(imagePath) {
+    // Grab the file name of the photo in the temporary directory
+    var currentName = imagePath.replace(/^.*[\\\/]/, '');
+ 
+    //Create a new name for the photo
+    var d = new Date(),
+    n = d.getTime(),
+    newFileName =  n + ".jpg";
+ 
+    // If you are trying to load image from the gallery on Android we need special treatment!
+    if ($cordovaDevice.getPlatform() == 'Android' && sourceType === Camera.PictureSourceType.PHOTOLIBRARY) {
+      window.FilePath.resolveNativePath(imagePath, function(entry) {
+        window.resolveLocalFileSystemURL(entry, success, fail);
+        function fail(e) {
+          console.error('Error: ', e);
+        }
+ 
+        function success(fileEntry) {
+          var namePath = fileEntry.nativeURL.substr(0, fileEntry.nativeURL.lastIndexOf('/') + 1);
+          // Only copy because of access rights
+          $cordovaFile.copyFile(namePath, fileEntry.name, cordova.file.dataDirectory, newFileName).then(function(success){
+            $scope.image = newFileName;
+          }, 
+          function(error){
+            $scope.showAlert('Error', error.exception);
+          });
+        };
+      });
+    } 
+    else {
+      var namePath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+      // Move the file to permanent storage
+      $cordovaFile.moveFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(function(success){
+        $scope.image = newFileName;
+      }, function(error){
+        $scope.showAlert('Error', error.exception);
+      });
+    }
+  },
+    function(err){
+      // Not always an error, maybe cancel was pressed...
+    })
+  }
+
+  $scope.pathForImage = function(image) {
+    if (image === null) {
+      return '';
+    } else {
+      return cordova.file.dataDirectory + image;
+    }
+  };
+
+  $scope.loadImage = function() {
+    var options = {
+      title: 'Seleccionar fuente de la Imagen',
+      buttonLabels: ['Cargar desde el tlf', 'Usar la camara'],
+      addCancelButtonWithLabel: 'Cancelar',
+      androidEnableCancelButton : true,
+    };
+    $cordovaActionSheet.show(options).then(function(btnIndex) {
+      var type = null;
+      if (btnIndex === 1) {
+        type = Camera.PictureSourceType.PHOTOLIBRARY;
+      } else if (btnIndex === 2) {
+        type = Camera.PictureSourceType.CAMERA;
+      }
+      if (type !== null) {
+        $scope.selectPicture(type);
+      }
+    });
+  };
 
   $scope.getGenders = function() {
 
